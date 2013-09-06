55194a2b1c724415fe70040d23f3d89d21924905
meh <meh@schizofreni.co>
2013-09-06 09:44:03 +0200

    Add support for gen_server errors

--- lib/derp/tty.ex
+++ lib/derp/tty.ex
@@ -7,6 +7,12 @@ defmodule Derp.TTY do
     "== #{title} for #{inspect pid} at #{DateTime.now |> DateTime.format %t"Y-m-d H:i:s"f} ==\r\n"
   end
 
+  defp print(leader, color, header, format, data) do
+    report = :io_lib.format(format, data)
+
+    IO.puts leader, IO.ANSI.escape("%{#{color}}" <> header <> String.from_char_list!(report), leader)
+  end
+
   defp print(leader, color, header, report) do
     report = if report |> is_binary do
       report
@@ -17,10 +23,22 @@ defmodule Derp.TTY do
     IO.puts leader, IO.ANSI.escape("%{#{color}}" <> header <> report, leader)
   end
 
-  defp print(leader, color, header, format, data) do
-    report = :io_lib.format(format, data)
+  defp print(:gen_server, leader, [name, msg, state, reason]) do
+    header       = header("Generic Server Error", name)
+    last_message = "-- Last Message: #{inspect msg, pretty: true}\r\n"
+    state        = "-- State: #{inspect state, pretty: true}\r\n"
+    reason       = "-- Reason: #{inspect reason, pretty: true}\r\n"
 
-    IO.puts leader, IO.ANSI.escape("%{#{color}}" <> header <> String.from_char_list!(report), leader)
+    IO.puts leader, IO.ANSI.escape("%{red}" <> header <> last_message <> state <> reason, leader)
+  end
+
+  @gen_server "** Generic server ~p terminating \n" <>
+              "** Last message in was ~p~n" <>
+              "** When Server state == ~p~n" <>
+              "** Reason for termination == ~n** ~p~n" |> String.to_char_list!
+
+  def handle_event({ :error, leader, { _pid, @gen_server, data } }, _state) do
+    print :gen_server, leader, data
   end
 
   def handle_event({ :error, leader, { pid, format, data } }, _state) do
