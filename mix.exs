d4ec0f6a03ffae2014592c325e549ae908991417
meh <meh@schizofreni.co>
2013-11-02 23:32:47 +0100

    mix: bump Elixir version requirement

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Derp.Mixfile do
   def project do
     [ app: :derp,
       version: "0.0.1",
-      elixir: "~> 0.10.3-dev",
+      elixir: "~> 0.10.3-dev or ~> 0.11.0",
       deps: deps ]
   end
 
